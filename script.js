4e3402ca1012a32a719ad51133d36c35a5beeff1
2001-skd <isanthakumardass@gmail.com>
2025-07-12 15:30:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const checkButton = document.querySelector(".check");
+const message = document.querySelector(".message");
+let scoreBoardVal = document.querySelector(".score");
+const highScore = document.querySelector(".highscore");
+const again = document.querySelector(".again");
+// console.log(score - 1);
+// const number = document.querySelector(".number");
+
+checkButton.addEventListener("click", () => {
+  // read the number when the user click the button , then only we can get the value , else empty string or 0.
+  const guessedNumber = Number(document.querySelector(".guess").value);
+  //   number.textContent = secretNumber;
+
+  if (!guessedNumber) {
+    // console.log("No Number");
+    message.textContent = "😫 No Number!";
+  }
+
+  if (guessedNumber > 0) {
+    if (secretNumber === guessedNumber) {
+      //   console.log("Guessed Number is Right !");
+      message.textContent = "😎 Guessed Number is Right !";
+      highScore.textContent = score;
+      document.body.style.background = "#60b347";
+    } else if (guessedNumber > secretNumber) {
+      //   console.log("Guessed Number is Too High !");
+      message.textContent = "🙄 Guessed Number is Too High !";
+      score > 0 ? score-- : 0;
+      //   console.log(score);
+      scoreBoardVal.textContent = score;
+    } else if (guessedNumber < secretNumber) {
+      message.textContent = "🙄 Guessed Number is Too Low !";
+      score > 0 ? score-- : 0;
+      //   console.log(score);
+
+      scoreBoardVal.textContent = score;
+    }
+  }
+
+  //   console.log(guessedNumber);
+});
+console.log(secretNumber);
+
+again.addEventListener("click", () => {
+  window.location.reload();
+});
